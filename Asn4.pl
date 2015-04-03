4d38602f26971a15bd0733771baf9bdb8dbee131
James Finlay <jtfinlay@ualberta.ca>
2015-04-03 14:45:58 -0600

    Tests done, all works. Time for comments.

--- Asn4.pl
+++ Asn4.pl
@@ -5,28 +5,6 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
-insert_data :-
-    assert(c325(fall_2014,aperf,15,15,15,15,79,99)),
-    assert(c325(fall_2014,john,14,13,15,10,76,87)),
-    assert(c325(fall_2014,lily, 9,12,14,14,76,92)),
-    assert(c325(fall_2014,peter,8,13,12,9,56,58)),
-    assert(c325(fall_2014,ann,14,15,15,14,76,95)),
-    assert(c325(fall_2014,ken,11,12,13,14,54,87)),
-    assert(c325(fall_2014,kris,13,10,9,7,60,80)),
-    assert(c325(fall_2014,audrey,10,13,15,11,70,80)),
-    assert(c325(fall_2014,randy,14,13,11,9,67,76)),
-    assert(c325(fall_2014,david,15,15,11,12,66,76)),
-    assert(c325(fall_2014,sam,10,13,10,15,65,67)),
-    assert(c325(fall_2014,kim,14,13,12,11,68,78)),
-    assert(c325(fall_2014,perf,15,15,15,15,80,100)),
-    assert(c325(winter_2014,aperf,15,15,15,15,80,99)),
-    assert(setup(fall_2014,as1,15,0.1)),
-    assert(setup(fall_2014,as2,15,0.1)),
-    assert(setup(fall_2014,as3,15,0.1)),
-    assert(setup(fall_2014,as4,15,0.1)),
-    assert(setup(fall_2014,midterm,80,0.25)),
-    assert(setup(fall_2014,final,100,0.35)).
-
 %*******************************************************************
 % Q1a **************************************************************
 % query1(+Semester,+Name, -Total) : Given a semester and a student 
@@ -53,16 +31,16 @@ query1(Semester,Name,Total) :-
 
 query2(Semester, L) :-
     findall(X, c325(Semester,X,_,_,_,_,_,_), L1),
-    !, constraint2(Semester, L2, L).
+    constraint2(Semester, L1, L).
 
 constraint2(_,[],[]).
 constraint2(Semester, [H|T], [H|R]) :-
     c325(Semester,H,_,_,_,_,M,F),
     setup(Semester,midterm,MM,_),
     setup(Semester,final,MF,_),
-    (F/MF) > (M/MM), !,
+    (F/MF) > (M/MM),
     constraint2(Semester,T,R).
-constraint2(Semester,[H|T],R) :- !, constraint2(Semester,T,R).
+constraint2(Semester,[H|T],R) :- constraint2(Semester,T,R).
 
 %*******************************************************************
 % Q1c **************************************************************
@@ -85,18 +63,8 @@ query3(_,_,_,_) :- print('record not found').
 
 %*******************************************************************
 % Q2 ***************************************************************
-% schedule(-TimeLst,-RmList) :  
-
-insert_data2 :-
-    assert(room(r1)),
-    assert(room(r2)),
-    assert(room(r3)),
-    assert(room(r4)),
-    assert(notAtSameTime([b,c,k])),
-    assert(before(k,f)),
-    assert(before(c,d)),
-    assert(at(a,_,r2)),
-    assert(at(d,_,r4)).
+% schedule(-TimeLst,-RmList) : Given previous assertions, determine 
+% a schedule that meets all room and time constraints.
 
 schedule(TimeList,RmList) :-
     TimeList = [A,B,C,D,E,F,G,H,I,J,K],
@@ -122,39 +90,33 @@ schedule(TimeList,RmList) :-
 
     List = [a,b,c,d,e,f,g,h,i,j,k],
     myPrint(TimeList,RmList,List). 
+schedule(_,_) :- write('cannot be scheduled'), false.
 
-schedule(_,_) :- write('cannot be scheduled').
+%*******************************************************************
+% myPrint(+TimeList, RmList, List) : Print out schedule in the TA-
+% approved format.
 
 myPrint([],[],[]). 
-myPrint([T|L],[W|R],[List|Rest]):- 
+myPrint([T|L],[W1|R],[List|Rest]):- 
+    W is W1+9,
     write('session '), write(List), write(' at time '), 
     write(T), write(' in room '), write(W), write('\n'), 
     myPrint(L,R,Rest).
 
 %*******************************************************************
-% constr1(+TimeList, +C1) :- Satisfy all "notAtSameTime constraints"
+% constr1(+TimeList, +C1) : Satisfy all "notAtSameTime constraints"
 
 constr1(_,[]).
 constr1(L, [H|C]) :-
     Tmp = [a,b,c,d,e,f,g,h,i,j,k],
     map_index(H,Tmp,I),
     map_values(I,L,V),
-    unique_r(V),
+    all_different(V),
     constr1(L,C).
 
-unique_r([]).
-unique_r([H|T]) :-
-    unique(H,T),
-    unique_r(T).
-
-unique(_,[]).
-unique(E,[H|T]) :-
-    E #\= H,
-    unique(E,T).
-
-
 %*******************************************************************
-% constr2(+TimeList, +C2) :- Satisfy all "before constraints"
+% constr2(+TimeList, +C2) : Satisfy all "before constraints"
+
 constr2(_,[]).
 constr2(L, [H|C]) :-
     Tmp = [a,b,c,d,e,f,g,h,i,j,k],
@@ -164,32 +126,39 @@ constr2(L, [H|C]) :-
     constr2(L,C).
 
 %*******************************************************************
-% constr3(+TimeLst, +RmList, +C3) :- Satisfy all "at constraints"
+% constr3(+TimeLst, +RmList, +C3) : Satisfy all "at constraints"
+
 constr3(_,_,[]).
 constr3(TL,RL,[HC|TC]) :-
     constr3_rooms(RL,HC),
     constr3_times(TL,HC),
     constr3(TL,RL,TC).
 
-% constr3_rooms(+RoomList,+Constraint) :- Satisfy room constraint
+% constr3_rooms(+RoomList,+Constraint) : Satisfy room constraint
+
 constr3_rooms(L,[S,_,R]) :-
-    Tmp = [a,b,c,d,e,f,g,h,i,j,k],
-    indexOf(S,Tmp,I),
+    Sessions = [a,b,c,d,e,f,g,h,i,j,k],
+    indexOf(S,Sessions,I),
     nth0(I,L,V),
     V #= R.
 
-% constr3_times(+TimeList,+Constraint) :- Satisfy time constraint
+% constr3_times(+TimeList,+Constraint) : Satisfy time constraint
+
 constr3_times(L,[S,T,_]) :-
     Sessions = [a,b,c,d,e,f,g,h,i,j,k],
-    indexOf(S,Sessions,IS),
-    nth0(IS,L,V),
-
     Times = [firstDayAm,firstDayPm,secondDayAm,secondDayPm],
-    indexOf(T,Times,IT),
 
-    V #= IT.
+    indexOf(S,Sessions,I1),
+    nth0(I1,L,V),
 
-% mapC3(C3i, RL, C3) : 
+    indexOf(T,Times,I2),
+    I3 is I2+1,
+
+    V #= I3.
+
+% mapC3(C3i, RL, C3) : Map the C3 constraint room names to an 
+% integer representation
+
 mapC3([],_,[]).
 mapC3([[A,B,E]|T], RL, [[A,B,I]|R]) :-
     indexOf(E,RL,Ii),
@@ -197,41 +166,49 @@ mapC3([[A,B,E]|T], RL, [[A,B,I]|R]) :-
     mapC3(T,RL,R).
 
 %*******************************************************************
-% map_index(+Values,+List,-Indices) : remap all 'Values' to their positions in 'List'
+% map_index(+Values,+List,-Indices) : remap all 'Values' to their 
+% positions in 'List'
+
 map_index([],_,[]).
 map_index([H|T],L,[I|R]) :-
     indexOf(H,L,I),
     map_index(T,L,R).
 
-% map_values(+Indicies,+List,-Values) : remap all 'Indicies' to their values in 'List'
+% map_values(+Indicies,+List,-Values) : remap all 'Indicies' to 
+% their values in 'List'
+
 map_values([],_,[]).
 map_values([H|T],L,[V|R]) :-
     nth0(H,L,V),
     map_values(T,L,R).
 
 % indexOf(+Value,+List,-Index) : determine the index of value in list
-indexOf(E,[E|_],0) :- !.
+
+indexOf(E,[E|_],0).
 indexOf(V,[_|T],Index) :-
     indexOf(V,T,Index1),
-    !,
     Index is Index1+1.
 
-%*******************************************************************
-% Ensure no A value is allocated twice for each B
+% exclusive(+A,+B) : For two lists, ensure pairs of values are unique
 
 exclusive(A,B) :-
     zip(A,B,Z),
     runique_pairs(Z).
+
+% runique_pairs(+L) : For given tuple list, ensure pairs are unique
 
 runique_pairs([]).
 runique_pairs([H|T]) :-
     unique_pairs(H,T),
     runique_pairs(T).
 
+% unique_pairs(+E,+L) : Ensure E does not exist in L
 unique_pairs(_,[]).
 unique_pairs([E1,E2],[[H1,H2]|T]) :-
     (E1 #\= H1 #\/ E2 #\= H2),
     unique_pairs([E1,E2],T).
+
+% zip(+A,+B,-L) : Zip A & B into tuple list L
 
 zip([],[],[]).
 zip([X|Xs], [Y|Ys], [[X,Y]|Zs]) :- zip(Xs,Ys,Zs).
@@ -239,7 +216,8 @@ zip([X|Xs], [Y|Ys], [[X,Y]|Zs]) :- zip(Xs,Ys,Zs).
 
 %*******************************************************************
 % Q3 ***************************************************************
-% subsetSum(+L,-Result) : 
+% subsetSum(+L,-Result) : Given a set of integers, is there a non-
+% empty subset whose sum is zero?
 
 subsetSum(L,Result) :-
     length(L, Len),
@@ -271,11 +249,3 @@ sum([],0).
 sum([H|T],R) :-
     sum(T,R1),
     R is H+R1.
-
-
-
-
-
-
-
-
---
+++ Tests.pl
@@ -0,0 +1,247 @@
+
+
+
+%*******************************************************************
+%*************************** Q1 DATA *******************************
+%*******************************************************************
+
+insert_data1 :-
+    assert(c325(fall_2014,aperf,15,15,15,15,79,99)),
+    assert(c325(fall_2014,john,14,13,15,10,76,87)),
+    assert(c325(fall_2014,lily, 9,12,14,14,76,92)),
+    assert(c325(fall_2014,peter,8,13,12,9,56,58)),
+    assert(c325(fall_2014,ann,14,15,15,14,76,95)),
+    assert(c325(fall_2014,ken,11,12,13,14,54,87)),
+    assert(c325(fall_2014,kris,13,10,9,7,60,80)),
+    assert(c325(fall_2014,audrey,10,13,15,11,70,80)),
+    assert(c325(fall_2014,randy,14,13,11,9,67,76)),
+    assert(c325(fall_2014,david,15,15,11,12,66,76)),
+    assert(c325(fall_2014,sam,10,13,10,15,65,67)),
+    assert(c325(fall_2014,kim,14,13,12,11,68,78)),
+    assert(c325(fall_2014,perf,15,15,15,15,80,100)),
+    assert(c325(winter_2014,aperf,15,15,15,15,80,99)),
+    assert(setup(fall_2014,as1,15,0.1)),
+    assert(setup(fall_2014,as2,15,0.1)),
+    assert(setup(fall_2014,as3,15,0.1)),
+    assert(setup(fall_2014,as4,15,0.1)),
+    assert(setup(fall_2014,midterm,80,0.25)),
+    assert(setup(fall_2014,final,100,0.35)).
+
+retract_data1 :-
+    retract(c325(fall_2014,aperf,15,15,15,15,79,99)),
+    retract(c325(fall_2014,john,14,13,15,10,76,87)),
+    retract(c325(fall_2014,lily, 9,12,14,14,76,92)),
+    retract(c325(fall_2014,peter,8,13,12,9,56,58)),
+    retract(c325(fall_2014,ann,14,15,15,14,76,95)),
+    retract(c325(fall_2014,ken,11,12,13,14,54,87)),
+    retract(c325(fall_2014,kris,13,10,9,7,60,80)),
+    retract(c325(fall_2014,audrey,10,13,15,11,70,80)),
+    retract(c325(fall_2014,randy,14,13,11,9,67,76)),
+    retract(c325(fall_2014,david,15,15,11,12,66,76)),
+    retract(c325(fall_2014,sam,10,13,10,15,65,67)),
+    retract(c325(fall_2014,kim,14,13,12,11,68,78)),
+    retract(c325(fall_2014,perf,15,15,15,15,80,100)),
+    retract(c325(winter_2014,aperf,15,15,15,15,80,99)),
+    retract(setup(fall_2014,as1,15,0.1)),
+    retract(setup(fall_2014,as2,15,0.1)),
+    retract(setup(fall_2014,as3,15,0.1)),
+    retract(setup(fall_2014,as4,15,0.1)),
+    retract(setup(fall_2014,midterm,80,0.25)),
+    retract(setup(fall_2014,final,100,0.35)).
+
+%*******************************************************************
+%*************************** Q2 TESTS ******************************
+%*******************************************************************
+
+test3_1 :-
+    assert(room(r1)),
+    assert(room(r2)),
+    assert(room(r3)),
+    assert(notAtSameTime([b,i,h,g])),
+    assert(before(i,j)),
+    assert(at(a,_,r2)),
+
+    (schedule(T,R);true),
+
+    retract(room(r1)),
+    retract(room(r2)),
+    retract(room(r3)),
+    retract(notAtSameTime([b,i,h,g])),
+    retract(before(i,j)),
+    retract(at(a,_,r2)).
+
+test3_2 :-
+    assert(room(r1)),
+    assert(room(r2)),
+    assert(room(r3)),
+    assert(room(r4)),
+    assert(notAtSameTime([b,c,k])),
+    assert(before(k,f)),
+    assert(before(c,d)),
+    assert(at(a,_,r2)),
+    assert(at(d,_,r4)),
+
+    (schedule(T,R);true),
+
+    retract(room(r1)),
+    retract(room(r2)),
+    retract(room(r3)),
+    retract(room(r4)),
+    retract(notAtSameTime([b,c,k])),
+    retract(before(k,f)),
+    retract(before(c,d)),
+    retract(at(a,_,r2)),
+    retract(at(d,_,r4)).
+
+test3_3 :-
+    assert(room(r1)),
+    assert(room(r2)),
+    assert(room(r3)),
+    assert(room(r4)),
+    assert(notAtSameTime([b,g,k])),
+    assert(before(k,f)),
+    assert(before(c,d)),
+    assert(at(a,firstDayPm,_)),
+    assert(at(d,_,r4)),
+
+    (schedule(T,R);true),
+
+    retract(room(r1)),
+    retract(room(r2)),
+    retract(room(r3)),
+    retract(room(r4)),
+    retract(notAtSameTime([b,g,k])),
+    retract(before(k,f)),
+    retract(before(c,d)),
+    retract(at(a,firstDayPm,_)),
+    retract(at(d,_,r4)).
+
+%*******************************************************************
+% test3_4
+% Session will be held in five different rooms.  
+% Session d,g,h,k cannot be held at the same time.  
+% Session a,c cannot be held at the same time.  
+% Session k should precede d.  
+% Session d should precede j.  
+% Session a is schedule at any time in room r2.  
+% Session c is schedule at any time in room r3.  
+
+test3_4 :-
+    assert(room(r1)),
+    assert(room(r2)),
+    assert(room(r3)),
+    assert(room(r4)),
+    assert(room(r5)),
+    assert(notAtSameTime([d,g,h,k])),
+    assert(notAtSameTime([a,c])),
+    assert(before(k,d)),
+    assert(before(d,j)),
+    assert(at(a,_,r2)),
+    assert(at(c,_,r3)),
+
+    (schedule(T,R);true),
+
+    retract(room(r1)),
+    retract(room(r2)),
+    retract(room(r3)),
+    retract(room(r4)),
+    retract(room(r5)),
+    retract(notAtSameTime([d,g,h,k])),
+    retract(notAtSameTime([a,c])),
+    retract(before(k,d)),
+    retract(before(d,j)),
+    retract(at(a,_,r2)),
+    retract(at(c,_,r3)).
+
+%*******************************************************************
+% test3_5
+% Session will be held in five different rooms.  
+% Session e,f,i,j cannot be held at the same time.  
+% Session f should precede j.   
+% Session d should precede f.  
+% Session j should precede d.  
+% Session f is schedule at any time in room r4.  
+
+test3_5 :-
+    assert(room(r1)),
+    assert(room(r2)),
+    assert(room(r3)),
+    assert(room(r4)),
+    assert(room(r5)),
+    assert(notAtSameTime([e,f,i,j])),
+    assert(before(f,j)),
+    assert(before(j,d)),
+    assert(before(d,f)),
+    assert(at(f,_,r4)),
+
+    (schedule(T,R);true),
+
+    retract(room(r1)),
+    retract(room(r2)),
+    retract(room(r3)),
+    retract(room(r4)),
+    retract(room(r5)),
+    retract(notAtSameTime([e,f,i,j])),
+    retract(before(f,j)),
+    retract(before(j,d)),
+    retract(before(d,f)),
+    retract(at(f,_,r4)).
+
+test3_6 :-
+    assert(room(r1)),
+    assert(room(r2)),
+    assert(room(r3)),
+    assert(room(r4)),
+    assert(room(r5)),
+    assert(room(r6)),
+    assert(notAtSameTime([e,f,i,j])),
+    assert(notAtSameTime([d,c,k])),
+    assert(before(f,j)),
+    assert(before(k,d)),
+    assert(at(a,_,r6)),
+    assert(at(f,_,r4)),
+    assert(at(g,_,r1)),
+
+    (schedule(T,R);true),
+
+    retract(room(r1)),
+    retract(room(r2)),
+    retract(room(r3)),
+    retract(room(r4)),
+    retract(room(r5)),
+    retract(room(r6)),
+    retract(notAtSameTime([e,f,i,j])),
+    retract(notAtSameTime([d,c,k])),
+    retract(before(f,j)),
+    retract(before(k,d)),
+    retract(at(a,_,r6)),
+    retract(at(f,_,r4)),
+    retract(at(g,_,r1)).
+
+
+test3_7 :-
+    assert(room(r1)),
+    assert(room(r2)),
+    assert(room(r3)),
+    assert(room(r4)),
+    assert(room(r5)),
+    assert(notAtSameTime([d,h,k])),
+    assert(notAtSameTime([a,b,g])),
+    assert(before(k,d)),
+    assert(before(d,j)),
+    assert(at(a,_,r2)),
+    assert(at(c,secondDayPm, r4)),
+
+    schedule(R,T),
+
+    retract(room(r1)),
+    retract(room(r2)),
+    retract(room(r3)),
+    retract(room(r4)),
+    retract(room(r5)),
+    retract(notAtSameTime([d,h,k])),
+    retract(notAtSameTime([a,b,g])),
+    retract(before(k,d)),
+    retract(before(d,j)),
+    retract(at(a,_,r2)),
+    retract(at(c,secondDayPm, r4)).
